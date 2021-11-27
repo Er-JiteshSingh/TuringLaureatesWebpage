80d8812a39d007e981727489408ece7087b29472
Jitesh Singh <generalpurposeduke@gmail.com>
2021-11-27 18:24:32 +0530

    Added Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllanTuringComponent } from './allan-turing/allan-turing.component';
+import { HomeComponent } from './home/home.component';
+import { TuringAwardeesComponent } from './turing-awardees/turing-awardees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",
+    component:HomeComponent,
+    pathMatch:"full"
+  },
+  {
+    path:"allen",
+    component:AllanTuringComponent,
+    pathMatch:"full"
+  },
+  {
+    path:"awardee",
+    component:TuringAwardeesComponent,
+    pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
